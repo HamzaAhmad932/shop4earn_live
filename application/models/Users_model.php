c5c1dedf58fac3ff272f22fd32b39b01f52219b3
Hamza Ahmad <hamza.ahmad0932@gmail.com>
2019-12-11 00:18:44 +0500

    withdraw payment issue as direct bonus is not in sum of total amount is fixed

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -174,20 +174,14 @@ class Users_model extends CI_Model{
         return $query->row()->payout;
     }
     public function chk_current_bal($user_id){
-         $this->db->select_sum('comission');
+        
+        $this->db->select('*');
         $this->db->from('users');
         $this->db->where('user_id',$user_id);
         $this->db->where('(type = 2) ');
         $query = $this->db->get();
-        $base_share_amount=$query->row()->comission;
-        $this->db->select_sum('booster_com');
-        $this->db->from('users');
-        $this->db->where('user_id',$user_id);
-        $this->db->where('(type = 2) ');
-        $query = $this->db->get();
-        $boster_share_amount=$query->row()->booster_com;
-        $chk_current_bal = $base_share_amount + $boster_share_amount;
-        return $chk_current_bal;
+        $user = $query->row();
+        return $user->comission + $user->booster_com + $user->direct_bonus;
     }
     public function insert_payment_request($data){
         $this->db->insert('pending_payments',$data);
@@ -244,7 +238,7 @@ class Users_model extends CI_Model{
         if($query->num_rows()){
             $result = $query->row();
 
-            $total_amount = $result->comission + $result->booster_com;
+            $total_amount = $result->comission + $result->booster_com + $result->direct_bonus;
             $available_amount = $total_amount - $amount;
 
             return $available_amount < 0 ? false : true;
